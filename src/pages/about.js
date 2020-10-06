de020178e8b024c0095e924c48a2d347c20bdf03
Rune Fjellheim <rune@fjellheim.tv>
2020-10-06 21:44:51 +0200

    Header og tittel og 404

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import Head from "../components/head"
 
 const AboutPage = () => {
     return (
         <Layout>
+            <Head title="About" />
             <h1>Rune Fjellheim</h1>
             <p>Jeg er foreløpig mellom jobber.</p>
             <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
